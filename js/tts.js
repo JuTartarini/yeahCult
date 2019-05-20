d1d2267b8ee079deee558a323a66800bf6710dbe
hlays <lays.hidani@gmail.com>
2019-05-20 14:55:36 -0300

    Implementado fetch

--- js/tts.js
+++ js/tts.js
@@ -1,8 +1,19 @@
-const text = 'hey, Olá, tudo pronto para começar a conhecer o mundo ao seu redor?';
+const text = 'Hey, Olá, tudo pronto para começar a conhecer o mundo ao seu redor?';
 const url = 'http://api.voicerss.org/';
 const apiKey = '4f7e97301b1f4542b90ca9ffaa0c9e81';
-const playBtn = document.getElementById('play-btn');
+const errorMsg = document.getElementById('error');
 const player = document.getElementById('player');
-const endpoint = `${url}?key=${apiKey}&hl=pt-BR&src=${text}&f=48khz_16bit_stereo`;
 
-player.setAttribute('src', endpoint);
+fetch(`${url}?key=${apiKey}&hl=pt-BR&src=${text}&f=48khz_16bit_stereo`)
+  .then(resp => {
+    player.preload = 'auto';
+    const source = document.createElement('source');
+    source.type = 'audio/wav';
+    source.src = resp.url;
+
+    player.appendChild(source);
+  })
+  .catch(error => {
+    errorMsg.textContent = error + 'Erro. Não foi possível carregar o áudio!';
+  });
+
