58e09fc330cbfd15d28fdfd294d38c139dc7f4a7
Rebeca <rebeca.felix@hotmail.com>
2019-05-21 18:39:50 -0300

    translate done

--- js/api-wiki.js
+++ js/api-wiki.js
@@ -1,5 +1,7 @@
 const place = 'Museu_de_Arte_de_São_Paulo';
 const wikiUrl = 'https://pt.wikipedia.org/api/rest_v1/page/summary';
+const wikiUrlEn = 'https://en.wikipedia.org/api/rest_v1/page/summary';
+
 const url =
 
   fetch(`${wikiUrl}/${place}`)
@@ -23,3 +25,28 @@ const url =
       fetchTTS(wikiSummary);
     });
 
+$("#btnEnglish").on("click", () => {
+  $("#summary").text("")
+
+    fetch(`${wikiUrlEn}/${place}`)
+      .then(resp => resp.json())
+      .then(resp => {
+        const wikiSummary = resp.extract;
+        const newP = document.createElement('p');
+        newP.textContent = wikiSummary;
+        newP.setAttribute('id', 'text');
+        newP.setAttribute('class', 'text');
+        summary.appendChild(newP);
+
+        const moreUrl = resp.content_urls.mobile.page;
+        const newA = document.createElement('a');
+        newA.setAttribute('class', 'read-more');
+        newA.textContent = 'Know more...';
+        newA.href = moreUrl;
+        newA.target = '_blank';
+        summary.appendChild(newA);
+
+        fetchTTS(wikiSummary);
+      });
+
+})
