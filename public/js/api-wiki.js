c074a0c6eb0045bc7e2cde4e7046015240880f1f
hlays <lays.hidani@gmail.com>
2019-05-21 22:37:21 -0300

    Add photos

--- public/js/api-wiki.js
+++ public/js/api-wiki.js
@@ -1,5 +1,6 @@
-let place = 'Parque_Trianon';
+const place = 'Parque_Trianon';
 const wikiUrl = 'https://pt.wikipedia.org/api/rest_v1/page/summary';
+const wikiUrlEn = 'https://en.wikipedia.org/api/rest_v1/page/summary';
 
 fetch(`${wikiUrl}/${place}`)
   .then(resp => resp.json())
@@ -34,39 +35,27 @@ fetch(`${wikiUrl}/${place}`)
     fetchTTS(wikiSummary);
   });
 
-let photos = {
-  'Fundação_Cásper_Líbero': ['../imgs/fundaçao-casper-libero.jpg', '../imgs/fundaçao-casper-libero-2.jpeg'],
-  'Conjunto_Nacional': ['../imgs/conjunto-nacional.jpg', '../imgs/conjunto-nacional-2.jpg', '../imgs/conjunto-nacional-3.jpg', '../imgs/conjunto-nacional-4.jpg'],
-  'Parque_Trianon': ['../imgs/parque-trianon.jpg', '../imgs/parque-trianon-2.jpg', '../imgs/parque-trianon-3.jpg'],
-  'Edifício_Anchieta': ['../imgs/edificio_anchieta.jpg', '../imgs/edificio_anchieta-2.jpg', '../imgs/edificio_anchieta-3.jpg'],
-  'Museu_de_Arte_de_São_Paulo': ['../imgs/masp.jpg', '../imgs/masp-assis-chateaubriand.jpg', '../imgs/masp-pietro-bardi.jpg', '../imgs/masp-aerea.jpg']
-};
+$('#btnEnglish').on('click', () => {
+  $('#summary').text('');
 
-function showPhotos(place) {
-  console.log(photos[place]);
-  // for (photo in photos[place]) {
-  // gallery.innerHTML = ``;
-  gallery.innerHTML = `
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100 photo" src="${photos[place][0]}" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100 photo" src="${photos[place][1]}" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100 photo" src="${photos[place][2]}" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-      `;
-}
+  fetch(`${wikiUrlEn}/${place}`)
+    .then(resp => resp.json())
+    .then(resp => {
+      const wikiSummary = resp.extract;
+      const newP = document.createElement('p');
+      newP.textContent = wikiSummary;
+      newP.setAttribute('id', 'text');
+      newP.setAttribute('class', 'text');
+      summary.appendChild(newP);
+
+      const moreUrl = resp.content_urls.mobile.page;
+      const newA = document.createElement('a');
+      newA.setAttribute('class', 'read-more');
+      newA.textContent = 'Know more...';
+      newA.href = moreUrl;
+      newA.target = '_blank';
+      summary.appendChild(newA);
+
+      fetchTTS(wikiSummary);
+    });
+})
