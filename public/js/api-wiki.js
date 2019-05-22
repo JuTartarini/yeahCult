31ce0012c7e4d7fd487e02114db09f3c5f5b6a5a
hlays <lays.hidani@gmail.com>
2019-05-22 18:43:44 -0300

    Implementa api maps com wiki

--- public/js/api-wiki.js
+++ public/js/api-wiki.js
@@ -1,38 +1,11 @@
-const place = 'Parque_Trianon';
+const PLACE_NAME = window.location.search.match(/\?placeName=(.*)/)[1];
+
+// const place = 'Parque_Trianon';
 const wikiUrl = 'https://pt.wikipedia.org/api/rest_v1/page/summary';
 const wikiUrlEn = 'https://en.wikipedia.org/api/rest_v1/page/summary';
 
-fetch(`${wikiUrl}/${place}`)
-  .then(resp => resp.json())
-  .then(resp => {
-    const wikiTitle = resp.title;
-    const wikiSummary = resp.extract;
-    const moreUrl = resp.content_urls.mobile.page;
-
-    summary.innerHTML = `
-      <h1>${wikiTitle}</h1>
-      <p id='text' class='text'>${wikiSummary}</p>
-      <a class='read-more' href='${moreUrl}' target='_blank'>Saiba mais...</a>
-    `;
-
-    if (place === 'Museu_de_Arte_de_São_Paulo') {
-      showPhotos('Museu_de_Arte_de_São_Paulo');
-    } else if (place === 'Parque_Trianon') {
-      showPhotos('Parque_Trianon');
-    } else if (place === 'Conjunto_Nacional') {
-      showPhotos('Conjunto_Nacional');
-    } else if (place === 'Fundação_Cásper_Líbero') {
-      showPhotos('Fundação_Cásper_Líbero');
-    } else if (place === 'Edifício_Anchieta') {
-      showPhotos('Edifício_Anchieta');
-    }
-    // fetchTTS(wikiSummary);
-  });
-
-$('#btnEnglish').on('click', () => {
-  $('#summary').text('');
-
-  fetch(`${wikiUrlEn}/${place}`)
+const wiki = () => {
+  fetch(`${wikiUrl}/${PLACE_NAME}`)
     .then(resp => resp.json())
     .then(resp => {
       const wikiTitle = resp.title;
@@ -42,9 +15,43 @@ $('#btnEnglish').on('click', () => {
       summary.innerHTML = `
       <h1>${wikiTitle}</h1>
       <p id='text' class='text'>${wikiSummary}</p>
-      <a class='read-more' href='${moreUrl}' target='_blank'>Read more...</a>
+      <a class='read-more' href='${moreUrl}' target='_blank'>Saiba mais...</a>
     `;
 
-      // fetchTTS(wikiSummary);
+      if (PLACE_NAME === 'Museu_de_Arte_de_São_Paulo') {
+        showPhotos('Museu_de_Arte_de_São_Paulo');
+      } else if (PLACE_NAME === 'Parque_Trianon') {
+        showPhotos('Parque_Trianon');
+      } else if (PLACE_NAME === 'Conjunto_Nacional') {
+        showPhotos('Conjunto_Nacional');
+      } else if (PLACE_NAME === 'Fundação_Cásper_Líbero') {
+        showPhotos('Fundação_Cásper_Líbero');
+      } else if (PLACE_NAME === 'Edifício_Anchieta') {
+        showPhotos('Edifício_Anchieta');
+      }
+      fetchTTS(wikiSummary);
     });
-})
+
+  $('#btnEnglish').on('click', () => {
+    $('#summary').text('');
+
+    fetch(`${wikiUrlEn}/${PLACE_NAME}`)
+      .then(resp => resp.json())
+      .then(resp => {
+        const wikiTitle = resp.title;
+        const wikiSummary = resp.extract;
+        const moreUrl = resp.content_urls.mobile.page;
+
+        summary.innerHTML = `
+          <h1>${wikiTitle}</h1>
+          <p id='text' class='text'>${wikiSummary}</p>
+          <a class='read-more' href='${moreUrl}' target='_blank'>Read more...</a>
+        `;
+
+        fetchTTS(wikiSummary);
+      });
+  })
+}
+
+// wiki();
+console.log(wiki());
--- public/js/map.js
+++ public/js/map.js
@@ -2,30 +2,32 @@ function flip() {
   $('.card').toggleClass('flipped');
 }
 
-var map;
 
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {
-                    lat: -23.5629,
-                    lng: -46.6544
-                },
-                zoom: 15
-            });
-            let home = new google.maps.LatLng(-23.5629, -46.6544);
-            let locations = [
-                ['First Shoppe', -23.561414, -46.6580706],
-                ['Second Shoppe', -23.5629, -46.6544],
-                ['Third Shoppe', -23.558976, -46.6625623]
-            ]
+function initMap() {
 
-            for (i = 0; i < locations.length; i++) {
-                homeMarker = new google.maps.Marker({
-                    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-                    title: locations[i][0],
-                    map: map
-                });
-            }
-        }
+  let map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -23.5629,
+      lng: -46.6544
+    },
+    zoom: 15
+  });
+  let locations = [
+    ['Condomínio_Edifício Anchieta', -23.5552326, -46.6630951],
+    ['Museu_de_Arte_de_São_Paulo_Assis_Chateaubriand', -23.561414, -46.6558819],
+    ['Parque_Trianon', -23.5623276, -46.6573249],
+    ['Fundação_Cásper_Líbero', -23.565382, -46.6513127],
+    ['Conjunto_Nacional', -23.558976, -46.6603736]
+  ]
+  for (i = 0; i < locations.length; i++) {
+    let homeMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      title: locations[i][0],
+      map: map
+    })
+    homeMarker.addListener('click', function () {
+      window.location = `wiki.html?placeName=${homeMarker.title}`;
+    });
+  }
 
-       
+}
